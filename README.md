f20721484fed6c8530ed60149fbc53f2246f3649
jlew-cs <jlew@communispace.com>
2014-02-10 14:34:01 -0500

    Initial repro

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ SignalRIssue2864
 ================
 
 Minimal project reproduction of https://github.com/SignalR/SignalR/issues/2864
+
+To reproduce:
+
+1. Load the default page (SimpleConnect.html)
+2. See "connecting", "connected", "hello" status lines appear in the browser
+3. Wait about 10 seconds.
+4. Refresh the browser.
+5. App will display "connecting" and hang on the /signalr/connect request, until the client times out (the server side timeout is set to 5 minutes currently in SignalRStartup.cs)
---
+++ WebApplication1/SignalRStartup.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.Configuration;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using WebApplication1;
+
+[assembly:OwinStartup(typeof(SignalRStartup))]
+namespace WebApplication1
+{
+    public class FooHub : Hub
+    {
+        public string Hello()
+        {
+            return "hi";
+        }
+    }
+
+    public class SignalRStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConfig = new HubConfiguration
+            {
+#if DEBUG
+                EnableDetailedErrors = true
+#else
+                    EnableDetailedErrors = false
+#endif
+            };
+
+
+#if DEBUG
+            const bool defaultVal = true;
+#else
+    // Low-level logging off by default in release mode, can be overridden by app setting
+            const bool defaultVal = false;
+#endif
+            // Trace SignalR at a low level
+            //if (new ConfigProvider().GetBoolAppSetting("SignalRPipelineLogging", defaultVal))
+            //{
+            //    LoggingPipelineModule.EnableLogging();
+            //}
+
+            GlobalHost.DependencyResolver.UseSqlServer(
+                new SqlScaleoutConfiguration(
+                    ConfigurationManager.ConnectionStrings["CatalystMessaging"].ConnectionString));
+
+            GlobalHost.Configuration.TransportConnectTimeout = TimeSpan.FromMinutes(5);
+
+            // Branch the pipeline here for requests that start with "/signalr"
+            app.Map("/signalr", map =>
+            {
+                // Setup the cors middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch is already runs under the "/signalr"
+                // path.
+                map.RunSignalR(hubConfig);
+            });
+        }
+    }
+}
